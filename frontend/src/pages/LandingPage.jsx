8e7d6d818dfe6c6997137fbfe34b7ff5100c7c98
jhunblackbarber-sys <jhunblackbarber@gmail.com>
2025-12-20 17:49:07 -0500

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import minhaLogo from './assets/logo.png';
+import minhaLogo from './assets/LOGO.png';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
